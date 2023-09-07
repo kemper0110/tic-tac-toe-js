051155e3aeb3bd10995b605afdf7f797b4584ff2
kemper0110 <danil2003.2043@gmail.com>
2023-09-07 18:33:41 +0300

    3

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -18,14 +18,14 @@ export const board_state = Array.from({length: 15 * 15}, () => reactive({
 
 export function onCellClick(cell_id, cell_state) {
     return () => {
-        if(!try_set(game_state.player, cell_id))
+        if (!try_set(game_state.player, cell_id))
             return
         console.log('click', cell_id, cell_state.value)
         game_state.count += 1
         game_state.player = nextPlayer(game_state.player)
 
         const winner = who_wins()
-        if(winner === undefined) return
+        if (winner === undefined) return
 
         console.log("winner is", winner)
         // show modal with winner name
@@ -34,15 +34,15 @@ export function onCellClick(cell_id, cell_state) {
 }
 
 function nextPlayer(player) {
-    if(player === CellState.Zero)
+    if (player === CellState.Zero)
         return CellState.Cross
-    if(player === CellState.Cross)
+    if (player === CellState.Cross)
         return CellState.Zero
     return undefined
 }
 
 function try_set(player, position) {
-    if(board_state[position].value)
+    if (board_state[position].value)
         return false
     board_state[position].value = player
     return true
@@ -53,18 +53,58 @@ function try_set(player, position) {
 // null - nobody win
 // undefined - game not ended
 function who_wins() {
-    if(isWinning(CellState.Cross))
+    if (isWinning(CellState.Cross))
         return CellState.Cross
-    if(isWinning(CellState.Zero))
+    if (isWinning(CellState.Zero))
         return CellState.Zero
     const board_is_full = board_state.every(v => v.value !== CellState.Empty)
-    if(board_is_full)
+    if (board_is_full)
         return null
     return undefined
 }
 
 function isWinning(player) {
+    // rows, cols, diagonal-asc, diagonal-desc
 
+    const coordToIdx = (x, y) => x + y * 15
+
+    for (let y = 0; y < 15; ++y) {
+        for (let x = 0; x < 11; ++x) {
+            if (board_state[coordToIdx(x, y)].value === player) {
+                let counter = 0
+                for (let j = 0; j < 5 && board_state[coordToIdx(x + j, y)].value === player; ++j)
+                    ++counter
+                if (counter === 5)
+                    return true
+            }
+        }
+    }
+
+    for (let x = 0; x < 15; ++x) {
+        for (let y = 0; y < 11; ++y) {
+            if (board_state[coordToIdx(x, y)].value === player) {
+                let counter = 0
+                for (let j = 0; j < 5 && board_state[coordToIdx(x, y + j)].value === player; ++j)
+                    ++counter
+                if (counter === 5)
+                    return true
+            }
+        }
+    }
+
+    for (let i = 4; i < 11; ++i) {
+        for (let j = 0; j < i - 3; ++j) {
+            if(board_state[coordToIdx(j, i - j)].value === player) {
+                let counter = 0
+                for (let k = 0; k < 5 && board_state[coordToIdx(j, i - k)].value === player; ++k)
+                    ++counter
+                if (counter === 5)
+                    return true
+            }
+        }
+    }
+
+    return false
 }
 
 export function restart() {
